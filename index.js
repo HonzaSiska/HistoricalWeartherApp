54067daaf3bfc094d409088704785fb0daa553cb
HonzaSiska <siskajan@hotmail.com>
2023-04-09 18:24:27 +0200

    moved fetch function to different folder for test

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 require('dotenv').config()
 const cors = require('cors')
 const express = require('express');
-const { fetchData } = require("./util.js")
+// const { fetchData } = require("./util.js")
+
+const fetchData = async (date, station) => {
+    
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': process.env.XRapidAPIKey,
+            'X-RapidAPI-Host': process.env.XRapidAPIHost
+        }
+    };
+
+    try {
+        const fetchedData = await fetch(`https://meteostat.p.rapidapi.com/stations/hourly?station=${station}&start=${date}&end=${date}&tz=Europe%2FPrague&units=metric`, options)
+        const json = await fetchedData.json()
+        console.log('json', json)
+        return json
+    } catch (error) {
+        console.log('error',error)
+        return error
+    }
+
+    // fetch(`https://meteostat.p.rapidapi.com/stations/hourly?station=${station}&start=${date}&end=${date}&tz=Europe%2FPrague&units=metric`, options)
+    //     .then(response => response.json())
+    //     .then(response => {
+    //         //  console.log(response)
+    //          return response
+
+    //     })
+    //     .catch(err => console.error(err));
+}
+
 const app = express();
 app.use(cors())
 app.use(express.static(__dirname + '/public'));
