f1b1aac9fddfe856fdc0c1f0c45b7fffbe785bdc
HonzaSiska <siskajan@hotmail.com>
2023-04-09 21:14:54 +0200

    testing loop again

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const cors = require('cors')
 const express = require('express');
-const fetch = require('node-fetch');
+const fetchNode = require('node-fetch');
 
-const {fetchData} = require("./util.js");
+const { fetchData } = require("./util.js");
 
 const app = express();
 
@@ -16,19 +16,11 @@ app.use(express.json())
 app.post('/api/fetch', async (req, res) => {
     let fetchedData = []
 
-    const {dates, station} = req.body
-    console.log('body', process.env.XRapidAPIKey )
+    const { dates, station } = req.body
+    console.log('body', process.env.XRapidAPIKey)
     console.log('env1', process.env.XRapidAPIHost)
 
-    // const runLoop = async()=> {
-    //     for(let item of dates){
-    //         console.log('item', item)
-    //         const result =  await fetchData(item.date, station)
-    //         fetchedData.push(result)
-    //     }
-    // }
 
-    // await runLoop()
     const options = {
         method: 'GET',
         headers: {
@@ -37,32 +29,50 @@ app.post('/api/fetch', async (req, res) => {
         }
     };
 
+    // try {
+    //     const data = await fetch(`https://meteostat.p.rapidapi.com/stations/hourly?station=${station}&start=${dates[0].date}&end=${dates[0].date}&tz=Europe%2FPrague&units=metric`, options)
+    //     const json = await data.json()
+    //     console.log('json', json)
+    //     fetchedData.push(json)
+    //     console.log('result', JSON.stringify(fetchedData))
+
+    //     res.json(JSON.stringify(fetchedData));
+    // } catch (error) {
+    //     console.log('error',error)
+    //     return error
+    // }
+
+
+    // for(let item of dates){
+    //     console.log('item', item)
+    //     const result =  await fetchData(item.date, station)
+    //     fetchedData.push(result)
+
+    const runLoop = async () => {
+        for (let item of dates) {
+            const data = await fetchNode(`https://meteostat.p.rapidapi.com/stations/hourly?station=${station}&start=${item.date}&end=${item.date}&tz=Europe%2FPrague&units=metric`, options)
+            const json = await data.json()
+            console.log('json', json)
+            fetchedData.push(json)
+            console.log('result', JSON.stringify(fetchedData))
+        }
+    }
     try {
-        const data = await fetch(`https://meteostat.p.rapidapi.com/stations/hourly?station=${station}&start=${dates[0].date}&end=${dates[0].date}&tz=Europe%2FPrague&units=metric`, options)
-        const json = await data.json()
-        console.log('json', json)
-        fetchedData.push(json)
-        console.log('result', JSON.stringify(fetchedData))
-    
+        await runLoop()
         res.json(JSON.stringify(fetchedData));
     } catch (error) {
-        console.log('error',error)
-        return error
+        console.log(error)
     }
-   
+
     
 
-    // function delayedLoop() {
-    //     for (let i = 0; i < dates.length -1 ; i++) {
-    //       setTimeout(function() {
-    //         const result =   fetchData(dates[0].date, station)
-    //         fetchedData.push(result)
-    //       },( i + 1) * 334);
-    //     }
-    //   }
-      
-    //    delayedLoop();
-    
+    // console.log('result', JSON.stringify(fetchedData))
+
+    // res.json(JSON.stringify(fetchedData));
+
+
+
+
 });
 const PORT = process.env.PORT || 4000
-app.listen(PORT , () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
